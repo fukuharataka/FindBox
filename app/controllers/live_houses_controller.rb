0934858c8ec20f707716a9c4b2fb0e0071bed3ff
fukuharataka <chapio0817@gmail.com>
2019-01-16 08:18:12 +0000

    [create]ビューページとアクションの追加

--- app/controllers/live_houses_controller.rb
+++ app/controllers/live_houses_controller.rb
@@ -1,2 +1,41 @@
 class LiveHousesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit_basic
+  end
+
+  def edit_detail
+  end
+
+  def update_basic
+  end
+
+  def update_detail
+  end
+
+  def destroy
+  end
+
+private
+  def set_live_house
+    @live_house = Live_house.find(params[:id])
+  end
+
+  def detail_params
+    params.require(:live_house).permit(:detail)
+  end
+
+  def basic_params
+    params.require(:live_house).permit(:name, :hp_url, :capacity, :prefecture, :address, :phone_number, :image)
+  end
 end
