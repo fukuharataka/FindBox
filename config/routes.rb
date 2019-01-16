259a903df7ebb596a215f1eae16036117011bd38
fukuharataka <chapio0817@gmail.com>
2019-01-16 10:42:46 +0000

    [create]ライブハウス詳細情報の編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   root 'tops#top'
   resources :users, only:[:update, :show, :edit, :destroy]
-  get       'sign_in',            to: 'users#session_new',          as: 'sign_in'
-  post      'sign_in',            to: 'users#session_create'
-  get       'sign_up',            to: 'users#new',                  as: 'sign_up'
-  post      'sign_up',            to: 'users#create'
-  delete    'sign_out',           to: 'users#session_destroy',      as: 'sign_out'
+  get     'sign_in',             to: 'users#session_new',          as: 'sign_in'
+  post    'sign_in',             to: 'users#session_create'
+  get     'sign_up',             to: 'users#new',                  as: 'sign_up'
+  post    'sign_up',             to: 'users#create'
+  delete  'sign_out',            to: 'users#session_destroy',      as: 'sign_out'
 
   resources :live_houses, only:[:index, :show, :new, :create, :destroy]
-    get     '/:id/edit_basic',     to: 'live_houses#edit_basic',     as: 'edit_basic'
-    patch   '/:id/update_basic',   to: 'live_houses#update_basic',   as: 'update_basic'
-    put     '/:id/update_basic',   to: 'live_houses#update_basic'
-    get     '/:id/edit_detail',    to: 'live_houses#edit_detail',    as: 'edit_detail'
-    patch   '/:id/update_detail',  to: 'live_houses#update_detail',  as: 'update_detail'
-    put     '/:id/update_detail',  to: 'live_houses#update_detail'
+  get     '/:id/edit_basic',     to: 'live_houses#edit_basic',     as: 'edit_basic'
+  patch   '/:id/update_basic',   to: 'live_houses#update_basic',   as: 'update_basic'
+  put     '/:id/update_basic',   to: 'live_houses#update_basic'
+  get     '/:id/edit_detail',    to: 'live_houses#edit_detail',    as: 'edit_detail'
+  patch   '/:id/update_detail',  to: 'live_houses#update_detail',  as: 'update_detail'
+  put     '/:id/update_detail',  to: 'live_houses#update_detail'
 end
