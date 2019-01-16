ec97649ea8ca903954cea22877bbabfb496e4a70
fukuharataka <chapio0817@gmail.com>
2019-01-16 04:54:02 +0000

    [create]user新規登録機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
+
   end
 
   def edit
@@ -15,11 +17,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "新規登録をしました"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :nickname, :home_house)
   end
 end
