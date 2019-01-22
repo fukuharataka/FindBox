0021777f747974da3556342cc03eb452378692ea
fukuharataka <chapio0817@gmail.com>
2019-01-22 15:39:57 +0000

    [add]

--- app/models/live_house.rb
+++ app/models/live_house.rb
@@ -5,7 +5,5 @@ class LiveHouse < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: {in: 2..50}
   validates :capacity, presence: true
   validates :prefecture, presence: true
-  validates :hp_url, uniqueness: true, length: {maximum: 100}
-  validates :address, uniqueness: true, length: {in: 7..12}
 
 end
